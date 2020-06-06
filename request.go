e14085922ab0dbb9fe2499b5ddd4b9ff6160c8a6
Alvin Shaita <66435075+alvinshaita@users.noreply.github.com>
2020-06-06 10:18:40 +0300

    Update request.go

--- request.go
+++ request.go
@@ -14,48 +14,36 @@ func post(client_request string){
 	var strPost = []byte("POST")
 	
 	req := fasthttp.AcquireRequest()
-	// req.SetBody([]byte(`{"test": "pigeon","f":"fd"}`))
 	req.SetBody([]byte(client_request))
-
 	req.Header.SetMethodBytes(strPost)
 	req.SetRequestURIBytes(strRequestURI)
 	req.Header.SetContentType("application/json")
 	res := fasthttp.AcquireResponse()
+	
 	if err := fasthttp.Do(req, res); err != nil {
 		panic("handle error")
 	}
+	
 	fasthttp.ReleaseRequest(req)
-
-	// body := res.Body()
-	// fmt.Println(string(body))
-
-	// body=([]byte("this is completely new body contents"))
-	// Do something with body.
-
 	fasthttp.ReleaseResponse(res) // Only when you are done with body!
 }
 
 func get(){
 	var strPost = []byte("GET")
 	req := fasthttp.AcquireRequest()
-	// req.SetBody([]byte(`{"text": "test 1"}`))
 	req.Header.SetMethodBytes(strPost)
 	req.SetRequestURIBytes(strRequestURI)
 	req.Header.SetContentType("application/json")
 	res := fasthttp.AcquireResponse()
+	
 	if err := fasthttp.Do(req, res); err != nil {
 		panic("handle error")
 	}
 	fasthttp.ReleaseRequest(req)
-
 	body := res.Body()
-	
-
 	fasthttp.ReleaseResponse(res)
 	fmt.Println(string(body))
 }
-
-
 
 func main(){
 	if len(os.Args)==2{
@@ -68,7 +56,6 @@ func main(){
 	for i < 1000000 {
 		post(`{"text": "test `+strconv.Itoa(i)+`"}`)
 		get()
-
 		i+=1
 	}
 }
